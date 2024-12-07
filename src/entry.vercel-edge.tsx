17f032a0ac33bd941379428ebb42a352b56aa016
Glade <gladekettle@gmail.com>
2024-12-07 12:40:15 +1100

    feat: set run time at top

--- src/entry.vercel-edge.tsx
+++ src/entry.vercel-edge.tsx
@@ -1,9 +1,9 @@
+export const runtime = 'edge';
+
 import { createQwikCity, type PlatformVercel } from '@builder.io/qwik-city/middleware/vercel-edge';
 import qwikCityPlan from '@qwik-city-plan';
 import { manifest } from '@qwik-client-manifest';
 import render from './entry.ssr';
-
-export const runtime = 'edge';
 
 declare global {
   interface QwikCityPlatform extends PlatformVercel {}
